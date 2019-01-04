33b0f8a5876d275dc9df39956370ff84b8e35cb6
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-04 11:47:09 -0700

    move files

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -3,8 +3,7 @@ class Api::V1::GifsController < ApplicationController
 
   def index
     location = params[:location]
-    helper   = GifHelper.new(location)
-    gifs     = helper.daily_gifs
+    gifs   = GifHelper.new(location).daily_gifs
     render json: ForecastGifsSerializer.new(gifs)
   end
 
