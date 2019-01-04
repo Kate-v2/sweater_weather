383897282d6fec19bb90a2151362fa4b5f2ef460
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-04 10:01:10 -0700

    refactor stubs

--- spec/fixtures/stub_gif_day_denver.rb
+++ spec/fixtures/stub_gif_day_denver.rb
@@ -25,11 +25,23 @@ module StubGifDayDenver
       to_return(body: File.read(stub_gif_day1_denver_path))
   end
 
+
+
+  def stub_gif_day2_denver_path
+    # USING DAY 1 DATA
+    stub_gif_day1_denver_path
+  end
+
   def stub_gif_day2_denver
     term = "Clear throughout the day."
-    # USING DAY 1 DATA
     stub_request(:get, url(term)).
-      to_return(body: File.read(stub_gif_day1_denver_path))
+      to_return(body: File.read(stub_gif_day2_denver_path))
+  end
+
+
+
+  def stub_gif_day3_denver_path
+    "./spec/fixtures/api/v1/gif_days/denver_day3_gif.json"
   end
 
   def stub_gif_day3_denver
@@ -38,10 +50,22 @@ module StubGifDayDenver
       to_return(body: File.read(stub_gif_day3_denver_path))
   end
 
+
+
+  def stub_gif_day4_denver_path
+    "./spec/fixtures/api/v1/gif_days/denver_day4_gif.json"
+  end
+
   def stub_gif_day4_denver
     term = "Partly cloudy throughout the day."
     stub_request(:get, url(term)).
       to_return(body: File.read(stub_gif_day4_denver_path))
+  end
+
+
+
+  def stub_gif_day5_denver_path
+    "./spec/fixtures/api/v1/gif_days/denver_day5_gif.json"
   end
 
   def stub_gif_day5_denver
@@ -50,24 +74,42 @@ module StubGifDayDenver
       to_return(body: File.read(stub_gif_day5_denver_path))
   end
 
+
+
+  def stub_gif_day6_denver_path
+    "./spec/fixtures/api/v1/gif_days/denver_day6_gif.json"
+  end
+
   def stub_gif_day6_denver
     term = "Mostly cloudy until evening."
     stub_request(:get, url(term)).
       to_return(body: File.read(stub_gif_day6_denver_path))
   end
 
+
+
+  def stub_gif_day7_denver_path
+    # USING DAY 5 DATA
+    stub_gif_day5_denver_path
+  end
+
   def stub_gif_day7_denver
     term = "Mostly cloudy throughout the day."
-    # USING DAY 5 DATA
     stub_request(:get, url(term)).
-      to_return(body: File.read(stub_gif_day5_denver_path))
+      to_return(body: File.read(stub_gif_day7_denver_path))
+  end
+
+
+
+  def stub_gif_day8_denver_path
+    # USING DAY 3 DATA
+    stub_gif_day3_denver_path
   end
 
   def stub_gif_day8_denver
     term = "Mostly cloudy starting in the afternoon."
-    # USING DAY 3 DATA
     stub_request(:get, url(term)).
-      to_return(body: File.read(stub_gif_day3_denver_path))
+      to_return(body: File.read(stub_gif_day8_denver_path))
   end
 
 
