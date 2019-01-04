d2ce8f6f9c8729c6f4f8b491f3cf5d030d9090a6
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-03 23:15:17 -0700

    user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'Validation' do
+
+    it { should validate_presence_of   :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of   :password }
+
+  end
+
+
+end
