d1215a2bc7967c76e1a693be882c7b087c91e55c
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-04 10:58:38 -0700

    copyright is not part of gif

--- app/gifs/gif.rb
+++ app/gifs/gif.rb
@@ -5,12 +5,12 @@ class Gif
   attr_reader :url, :time, :summary, :copyright
 
   def initialize( day, url )
-    @url     = url
-    @day     = day
-    @time    = day.time
-    @summary = day.summary
-    # @copyright = 2018       #hard coded, but I don't currently know how to handle date format
-    @copyright = Date.now.year
+    @url       = url
+    @day       = day
+    @time      = day.time
+    @summary   = day.summary
+    # this is for the collection, not each
+    # @copyright = Date.today.year
   end
 
 
