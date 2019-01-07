e8433e6ad94a250f86d885a0d9f3af0b78376ab0
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-07 13:10:12 -0700

    reduce over abstraction

--- app/facades/gif_helper.rb
+++ app/facades/gif_helper.rb
@@ -35,45 +35,9 @@ class GifHelper
   end
 
   def get_gifs
-    GiphyService.new( daily_gif_target ).target_data
+    target = { target: :search, term: @_term }
+    GiphyService.new( target ).target_data
   end
 
-  def daily_gif_target
-    target = { target: :search, term: @_term }
-  end
 
 end
-
-
-# {
-#   data: {
-#     daily_forecasts: [
-#       {
-#         time: "1546498800",
-#         summary: "Mostly sunny in the morning.",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       },
-#       {
-#         time: "1546585200",
-#         summary: "Partly cloudy in the evening.",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       },
-#       {
-#         time: "1546671600",
-#         summary: "Snowy.",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       },
-#       {
-#         time: "1546758000",
-#         summary: "Firenado",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       },
-#       {
-#         time: "1546844400",
-#         summary: "Corgis because I cant think of other weather",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       }
-#     ]
-#   }
-#   copyright: "2018"
-# }
