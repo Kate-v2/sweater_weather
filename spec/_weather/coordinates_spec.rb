ba96f4fb59deeb505ec58f8b6eee226de275d22c
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-08 23:57:39 -0700

    coordinates gets spies

--- spec/_weather/coordinates_spec.rb
+++ spec/_weather/coordinates_spec.rb
@@ -22,6 +22,35 @@ describe "Coordinates" do
     expect(string).to eq(pair)
   end
 
+  describe 'Private or Class Methods' do
+
+    it 'private - does not lat' do
+      coord = spy('coordinates')
+      expect(coord).to have_not_received(:lat)
+    end
+
+    it 'private - does not lng' do
+      coord = spy('coordinates')
+      expect(coord).to have_not_received(:lng)
+    end
+
+    it 'private - does not city' do
+      coord = spy('coordinates')
+      expect(coord).to have_not_received(:city)
+    end
+
+    it 'private - does not state' do
+      coord = spy('coordinates')
+      expect(coord).to have_not_received(:state)
+    end
+
+    it 'private - does not country' do
+      coord = spy('coordinates')
+      expect(coord).to have_not_received(:country)
+    end
+
+  end
+
 end
 
 def make_geocode_data
