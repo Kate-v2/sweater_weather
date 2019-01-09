864cc535eb636d0486fe7cf0bcd6810648c6c76b
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-08 21:57:00 -0700

    add json test

--- spec/_weather/currently_spec.rb
+++ spec/_weather/currently_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 
 describe "Currently" do
 
-  let(:file)      { File.read(stub_dark_sky_denver_path) }
-  let(:data)      { JSON.parse( file, symbolize_names: true )[:currently] }
-  let(:currently) { Currently.new(data) }
+  let(:data)      { make_json }
+  let(:currently) { Currently.new( data ) }
 
   it 'it initializes from DarkSky sub-hash' do
     expect(currently.class).to eq(Currently)
@@ -19,4 +18,20 @@ describe "Currently" do
     expect(currently.feels_like).to  eq(data[:apparentTemperature])
   end
 
+  it 'makes a hash for json' do
+    json = currently.json
+    expect(json.class).to eq(Hash)
+    expect(json.keys.count).to eq(5)
+    expect(currently.time).to        eq(json[:time])
+    expect(currently.summary).to     eq(json[:summary])
+    expect(currently.icon).to        eq(json[:icon])
+    expect(currently.temperature).to eq(json[:temperature])
+    expect(currently.feels_like).to  eq(json[:feels_like])
+  end
+
+end
+
+def make_json
+  file = File.read(stub_dark_sky_denver_path)
+  JSON.parse( file, symbolize_names: true )[:currently]
 end
