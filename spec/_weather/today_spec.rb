8f6281998f0e422cce07fba0508d914fccdb86af
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-08 22:06:10 -0700

    add json test

--- spec/_weather/today_spec.rb
+++ spec/_weather/today_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 
 describe "Today" do
 
-  let(:file)  { File.read(stub_dark_sky_denver_path) }
-  let(:data)  { JSON.parse( file, symbolize_names: true )[:daily][:data][1] }
+  let(:data)  { make_json[:daily][:data][1] }
   let(:today) { Today.new(data) }
 
 
@@ -21,7 +20,24 @@ describe "Today" do
     expect(today.humidity).to   eq(data[:humidity])
     expect(today.visibility).to eq(data[:visibility])
     expect(today.uv_index).to   eq(data[:uvIndex])
-
   end
 
+  it 'makes a hash for json' do
+    json = today.json
+    expect(json.class).to       eq(Hash)
+    expect(today.time).to       eq(json[:time])
+    expect(today.summary).to    eq(json[:summary])
+    expect(today.icon).to       eq(json[:icon])
+    expect(today.low).to        eq(json[:low])
+    expect(today.high).to       eq(json[:high])
+    expect(today.humidity).to   eq(json[:humidity])
+    expect(today.visibility).to eq(json[:visibility])
+    expect(today.uv_index).to   eq(json[:uv_index])
+  end
+
+end
+
+def make_json
+  file = File.read(stub_dark_sky_denver_path)
+  JSON.parse( file, symbolize_names: true )
 end
