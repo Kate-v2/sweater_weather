995bd501113e1036321db9ee27d534a64d0e6604
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-08 22:00:43 -0700

    add json test

--- spec/_weather/day_spec.rb
+++ spec/_weather/day_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 
 describe "Day" do
 
-  let(:file) { File.read(stub_dark_sky_denver_path) }
-  let(:data) { JSON.parse( file, symbolize_names: true )[:daily][:data].first }
+  let(:data) { make_json[:daily][:data].first }
   let(:day)  { Day.new(data) }
 
   it 'it initializes from DarkSky sub-hash' do
@@ -12,7 +11,6 @@ describe "Day" do
   end
 
   it 'has attributes' do
-
     expect(day.time).to               eq(data[:time])
     expect(day.summary).to            eq(data[:summary])
     expect(day.icon).to               eq(data[:icon])
@@ -22,4 +20,21 @@ describe "Day" do
     expect(day.high).to               eq(data[:temperatureHigh])
   end
 
+  it 'makes a hash for json' do
+    json = day.json
+    expect(json.class).to eq(Hash)
+    expect(day.time).to               eq(json[:time])
+    expect(day.summary).to            eq(json[:summary])
+    expect(day.icon).to               eq(json[:icon])
+    expect(day.precip_probability).to eq(json[:precip_probability])
+    expect(day.precip_type).to        eq(json[:precip_type])
+    expect(day.low).to                eq(json[:low])
+    expect(day.high).to               eq(json[:high])
+  end
+
+end
+
+def make_json
+  file = File.read(stub_dark_sky_denver_path)
+  JSON.parse( file, symbolize_names: true )
 end
