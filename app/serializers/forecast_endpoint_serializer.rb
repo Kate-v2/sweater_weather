572831e8128af6a9ee9ab606de5dd6dd3c78d189
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-08 18:22:42 -0700

    delete overkill serializers

--- app/serializers/forecast_endpoint_serializer.rb
+++ app/serializers/forecast_endpoint_serializer.rb
@@ -6,27 +6,20 @@ class ForecastEndpointSerializer
   attribute :id
 
   attribute :current do |object|
-    # current = object.current
     current = object.current.json
-    # CurrentlySerializer.new(current)
   end
 
   attribute :today do |object|
-    # today = object.today
     today = object.today.json
-    # TodaySerializer.new(today)
   end
 
   attribute :forecast do |object|
-    # forecast = object.forecast.hours
     forecast = object.forecast
     ForecastSerializer.new(forecast)
   end
 
   attribute :location do |object|
     location = object.location.json
-    # location = object.location
-    # LocationSerializer.new(location)
   end
 
 
